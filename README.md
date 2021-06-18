15448359ab25a5cdcd622f2c5258260cd87c05ed
andrey <andreyvsolovov@outlook.com>
2021-06-18 22:24:19 +0100

    Ready

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dockerfile used in this work
---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+chmod 777 /work
+chmod 777 /ants_config
+cd /ants2/ANTS2/build
+. /opt/root/bin/thisroot.sh
+runuser --command='/bin/bash'
+#/bin/bash
+
